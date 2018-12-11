5c078421adfb4fdc5f62ae1a9b500b045b73efb8
Sierra Davis <sd.birdlover@gmail.com>
2018-12-11 02:14:17 +0000

    things are working but there are many things to change

--- cp6/public/javascripts/my_app.js
+++ cp6/public/javascripts/my_app.js
@@ -3,12 +3,14 @@
 angular.module('myApp', []).
   controller('myController', ['$scope', '$http',
                               function($scope, $http) {
-    $http.get('/user/profile')
-        .success(function(data, status, headers, config) {
+    $http.get('/user/profile') .success(function(data, status, headers, config) {
+      console.log(">>MyController: Got User: ", data);    
+      
       $scope.user = data;
       $scope.error = "";
-    }).
-    error(function(data, status, headers, config) {
+    }).error(function(data, status, headers, config) {
+      console.log(">>MyController: error during GET User: ", data);
+      
       $scope.user = {};
       $scope.error = data;
     });
