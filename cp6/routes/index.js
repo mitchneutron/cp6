74360f171d7eb237b8374375c4d4cc3dee7663b7
Sierra Davis <sd.birdlover@gmail.com>
2018-12-11 06:35:45 +0000

    Setting up to store/get chats or something in the DB

--- cp6/routes/index.js
+++ cp6/routes/index.js
@@ -1,23 +1,35 @@
 var express = require('express');
 var router = express.Router();
+// var mongoose = require('mongoose');  //copied from old lab until we decide what we're gonna store in the DB
+// var Candidate = mongoose.model('Candidate');  //also need to make a new model file for this object and register it in the db inside the server's app.js
 
 var users = require('../controllers/users_controller');
 
 router.get('/', function(req, res){
-    console.log("GET homepage called");
-//    console.log(req);
-    console.log(req.session);
+    console.log("GET homepage called, session= ");
+    console.log("/t" + req.session);
+    
     if (req.session.user) {
       console.log("GET homepage called - with user, color= ", req.session.color);
       res.render('index', {username: req.session.username,  //this is where the EJS vars are passed to the HTML
-                           msg:req.session.msg,
-                           color:req.session.color});
+                           msg:req.session.msg,            //I don't really know how else to send these back and still render a new page... 
+                           color:req.session.color});     // fine maybe EJS is the best way to do this part. ugh
     } else {
-      console.log("/ Route else user");
+      console.log("GET homepage: user session not active");
       req.session.msg = 'Access denied! Please log in first';
       res.redirect('/login');
     }
 });
+
+// Maybe here have a function that gets a list of chats/pics/whatever we end up 
+// storing from the database and returns it without redirecting to a new page
+/*
+router.get('/chats', function(req, res){
+  console.log("GET chats called");
+  
+  
+}
+*/
 
 
 router.get('/user', function(req, res){
@@ -45,7 +57,7 @@ router.get('/login',  function(req, res){
     if(req.session.user){
       res.redirect('/');
     }
-    res.render('login', {msg:req.session.msg});
+    res.render('login', {msg:req.session.msg}); 
 });
 
 
